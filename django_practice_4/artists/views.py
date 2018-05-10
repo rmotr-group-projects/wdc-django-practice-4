beef622b22773e31e650f098c15ba8d32fc0da9a
Ivan Zugnoni <zugnoni.ivan@gmail.com>
2018-05-10 16:19:56 -0300

    Remove solution

--- django_practice_4/artists/views.py
+++ django_practice_4/artists/views.py
@@ -6,29 +6,7 @@ from .forms import ArtistForm, SongForm
 
 
 def artists(request):
-    artists = Artist.objects.all()
-    if request.method == 'POST':
-        artist_form = ArtistForm(request.POST)
-        song_form = SongForm(request.POST)
-
-        if artist_form.is_valid():
-            Artist.objects.create(**artist_form.cleaned_data)
-            artist_form = ArtistForm()
-        elif song_form.is_valid():
-            song_form.save()
-            song_form = SongForm()
-    else:
-        artist_form = ArtistForm()
-        song_form = SongForm()
-    return render(
-        request,
-        'index.html',
-         context={
-            'artists': artists,
-            'artist_form': artist_form,
-            'song_form': song_form
-        }
-    )
+    pass
 
 
 def delete_song(request):
