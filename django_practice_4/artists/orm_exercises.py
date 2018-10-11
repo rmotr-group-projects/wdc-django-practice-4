07f3554d51186159ae37d81fc284a7f16fbde8ee
Trace <tessiert@unm.edu>
2018-10-11 15:03:35 -0600

    ORM tests passing

--- django_practice_4/artists/orm_exercises.py
+++ django_practice_4/artists/orm_exercises.py
@@ -8,8 +8,7 @@ def task_1_q_object_advanced_lookup():
         Should return all songs that contains the word 'Ground' in its
         title, OR its artist's artistic_name starts with Ed
     """
-    # HINT: Use django Q object to allow "OR" expresions in .filter()
-    pass
+    return Song.objects.filter(Q(title__icontains='Ground') | Q(artist__artistic_name__startswith='Ed'))
 
 
 def task_2_q_object_advanced_lookup():
@@ -17,15 +16,13 @@ def task_2_q_object_advanced_lookup():
         Should return all songs that starts with the letter 'S' OR a letter 'C'
         in its title, AND its artist's artistic_name contains a letter 'W'
     """
-    # HINT: Use django Q object to allow "OR" and "AND" expresions combined.
-    # Avoid using .filter() multiple times.
-    pass
+    return Song.objects.filter(Q(title__istartswith='S') | Q(title__istartswith='C'),
+        Q(artist__artistic_name__icontains='W'))
 
 
 def task_3_artists_update_popularity():
     """Should add 10 popularity points to all artists"""
-    # HINT: Use .update() function and django F object to refer to model's fields values
-    pass
+    Artist.objects.update(popularity=F('popularity') + 10)
 
 
 def task_4_songs_album_names():
@@ -33,4 +30,4 @@ def task_4_songs_album_names():
     # HINT: Use .values_list() function with 'flat' option, to get from the
     # database only the fields that you are interested in and not all the ones
     # in the model
-    pass
+    return Song.objects.values_list('album_name', flat=True).order_by('album_name')
