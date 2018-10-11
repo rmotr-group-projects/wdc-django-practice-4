0aa6221bb00f7c0492bb7292088f1666f2585db4
Trace <tessiert@unm.edu>
2018-10-11 13:40:37 -0600

    Add new artist form implemented

--- django_practice_4/artists/forms.py
+++ django_practice_4/artists/forms.py
@@ -4,8 +4,15 @@ from .models import GENRE_CHOICES, Song
 
 
 class ArtistForm(forms.Form):
-    pass
+    artistic_name = forms.CharField(label='Artistic Name', max_length=255)
+    first_name = forms.CharField(label='First Name', max_length=255, required=False)
+    last_name = forms.CharField(label='Last Name', max_length=255, required=False)
+    picture_url = forms.URLField(label='URL', required=False)
+    popularity = forms.IntegerField(label='Popularity', initial=0, required=False)
+    genre = forms.ChoiceField(label='Genre', choices=GENRE_CHOICES, required=False)
 
 
 class SongForm(forms.Form):
-    pass
+    artist = forms.CharField(label='Artist', max_length=255)
+    title = forms.CharField(max_length=255)
+    album_name = forms.CharField(max_length=255, required=False)
--- django_practice_4/artists/views.py
+++ django_practice_4/artists/views.py
@@ -9,8 +9,27 @@ def artists(request):
     # validate that user is authenticated, and if not, redirect to login page
     if not request.user.is_authenticated:
         return redirect('login')
-        
-    pass
+
+    # If it is a 'POST' request, make an instance of the form with the data
+    # provided and, if it is valid, create the Artist object
+    if request.method == 'POST':
+        add_artist_form = ArtistForm(request.POST)
+        if add_artist_form.is_valid():
+            Artist.objects.create(
+                artistic_name=add_artist_form.cleaned_data['artistic_name'],
+                first_name=add_artist_form.cleaned_data['first_name'],
+                last_name=add_artist_form.cleaned_data['last_name'],
+                picture_url=add_artist_form.cleaned_data['picture_url'],
+                popularity=add_artist_form.cleaned_data['popularity'],
+                genre=add_artist_form.cleaned_data['genre'])
+        return redirect('artists')
+    # For a GET (or any other method), make an instance of the form and return it,
+    # as well as a query set containing all artists, to the template as context
+    else:
+        add_artist_form = ArtistForm()
+        artists = Artist.objects.all()
+        return render(request, 'index.html', 
+            {'add_artist_form': add_artist_form, 'artists': artists})
 
 
 def delete_song(request):
